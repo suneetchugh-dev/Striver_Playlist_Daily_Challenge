99e9a6c89b14a4e45c66b0b6cb2c99d54c02352e
suneetchugh-dev <valorise078gramming@gmail.com>
2025-11-10 04:22:53 +0530

    Started Pattern Printing

---
+++ 2_pattern_printing.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+//can also use #include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //Rectangle Pattern
+    // for(int i=0; i<4; i++)
+    // {
+    //     for(int j=0; j<4; j++)
+    //     {
+    //         cout << "*" << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    //Increasing Triangle Pattern
+    // for(int i=0; i<5; i++)
+    // {
+    //     for(int j=0; j<=i; j++)
+    //     {
+    //         cout << "*" << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    //Increasing Number Triangle Pattern
+    // for(int i=0; i<5; i++)
+    // {
+    //     for(int j=0; j<=i; j++)
+    //     {
+    //         cout << j+1 << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    //Increasing Number Triangle Pattern 2
+    // for(int i=0; i<5; i++)
+    // {
+    //     for(int j=0; j<=i; j++)
+    //     {
+    //         cout << i << " ";
+    //     }
+    //     cout << endl;
+    // }
+}
