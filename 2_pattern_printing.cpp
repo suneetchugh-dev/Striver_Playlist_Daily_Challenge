70f1050c083a7b6d17cc997763b9c64b292d6c99
suneetchugh-dev <valorise078gramming@gmail.com>
2026-01-27 01:27:35 +0530

    Lec -5 Pattern Printing Done

--- 2_pattern_printing.cpp
+++ 2_pattern_printing.cpp
@@ -86,7 +86,7 @@ int main() {
   //    }
   //    cout << endl;
   //  }
-  
+
   // Reverse Center Triangle
   // int n = 5;
   // for (int i = 0; i < n; i++) {
@@ -98,29 +98,29 @@ int main() {
   //     cout << "*";
   //   cout << endl;
   // }
-  
-  //Diamond Pattern
-  // int n=4;
-  //  for(int i=0; i<n;i++)
-  //  {
-  //    //space
-  //    for(int j=0; j<n-i-1; j++)
-  //    {
-  //      cout << " ";
+
+  // Diamond Pattern
+  //  int n=4;
+  //   for(int i=0; i<n;i++)
+  //   {
+  //     //space
+  //     for(int j=0; j<n-i-1; j++)
+  //     {
+  //       cout << " ";
+  //      }
+  //      //star
+  //      for(int k=0; k<2*i+1; k++)
+  //     {
+  //       cout << "*";
   //     }
-  //     //star
-  //     for(int k=0; k<2*i+1; k++)
-  //    {
-  //      cout << "*";
+  //     //space
+  //     for(int j=0; j<n-i-1; j++)
+  //     {
+  //       cout << " ";
+  //      }
+  //      cout << endl;
   //    }
-  //    //space
-  //    for(int j=0; j<n-i-1; j++)
-  //    {
-  //      cout << " ";
-  //     }
-  //     cout << endl;
-  //   }
-    
+
   //   for (int i = 0; i < n; i++) {
   //     // leading spaces (increase each row)
   //     for (int j = 0; j < i; j++)
@@ -130,21 +130,127 @@ int main() {
   //       cout << "*";
   //     cout << endl;
   //   }
-    
-//Pattern 8 =>  Center Triangle Rotated 180 degrees
-//   int n=4;
-//   for(int i=0; i<=2*n-1;i++) 
+
+  // Pattern 8 =>  Center Triangle Rotated 180 degrees
+  //    int n=4;
+  //    for(int i=0; i<=2*n-1;i++)
+  //    {
+  //      int stars = i;
+  //      if (i>=n)
+  //      {
+  //        stars = 2*n - i;
+  //      }
+  //      for(int j=0; j<stars; j++)
+  //      {
+  //        cout << "*";
+  //      }
+  //      cout << endl;
+  //    }
+  //  }
+
+  // Pattern 9 => 1 & 0 Right Angle Pattern
+  //  int n =5;
+  //  for(int i=0; i<n; i++)
+  //  {
+  //    int start = 1;
+  //    for(int i=0; i<n; i++)
+  //    {
+  //      if(i%2 == 0)
+  //      {
+  //        start = 1;
+  //      }
+  //      else
+  //      {
+  //       start = 0;
+  //     }
+  //     for(int j=0; j<i; j++)
+  //     {
+  //       cout << start;
+  //       start = 1 - start;
+  //     }
+  //     cout << endl;
+  //   }
+  // }
+
+//Pattern 10 => Number Space Number Pattern
+  // int n = 5;
+  // for (int i = 1; i <= n; i++) {
+  //   int space = 2 * (n - i); // spaces shrink each row
+  //   // left ascending numbers 1..i
+  //   for (int j = 1; j <= i; j++) cout << j;
+  //   // middle spaces
+  //   for (int s = 0; s < space; s++) cout << " ";
+  //   // right descending numbers i..1
+  //   for (int j = i; j >= 1; j--) cout << j;
+  //   cout << endl;
+  // }
+
+
+//Increasing Number Pattern
+// int n=5;
+// int num = 1;
+// for(int i=0; i<=n; i++)
+// {
+//   for(int j=1; j<=i;j++)
 //   {
-//     int stars = i;
-//     if (i>=n) 
-//     {
-//       stars = 2*n - i;
-//     }
-//     for(int j=0; j<stars; j++)
-//     {
-//       cout << "*";
-//     }
-//     cout << endl;
+//     cout << num << " ";
+//     num = num+1;
 //   }
-// } 
-  // End Of File
+//   cout << endl;
+// }
+
+
+//Alphabet Pattern
+  // int n=5;
+  // for(int i=0;i<=n; i++)
+  // {
+  //   for(char ch='A'; ch<='A'+i; ch++)
+  //   {
+  //     cout << ch << " ";
+  //   }
+  //   cout<<endl;
+  // }
+
+  //Reverse Alphabet Pattern
+  // int n=5;
+  // for(int i=0; i<=n; i++)
+  // {
+  //   for(char ch='A'; ch<= 'A' + (n-i-1); ch++)
+  //   {
+  //     cout << ch << " ";
+  //   }
+  //   cout << endl;
+  // } 
+
+
+  //Row-wise Alphabet Repeating Pattern
+  // int n=5;
+  // for(int i=0; i<=n; i++)
+  // {
+  //   char ch='A'+ i;
+  //   for(int j=0; j<=i; j++)
+  //   {
+  //     cout << ch << " ";
+  //   }
+  //   cout << endl;
+  // }
+  
+//Empty Rectangle Pattern
+  // int n=5;
+  // for(int i=0; i<n; i++)
+  // {
+  //   for(int j=0; j<n; j++)
+  //   {
+  //     if(i==0 || j==0 || i==n-1 || j==n-1)
+  //     {
+  //       cout << "*";
+  //     }
+  //     else
+  //     {
+  //       cout << " ";
+  //     }
+  //   }
+  //   cout << endl;
+  // }
+}
+// End Of File
