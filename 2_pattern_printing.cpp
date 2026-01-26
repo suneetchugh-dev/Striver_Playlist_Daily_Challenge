6ef88424c2fb7fcb65eef647c44ccf6277600b21
suneetchugh-dev <valorise078gramming@gmail.com>
2026-01-26 16:59:01 +0530

    Center Triangle Pattern Done

--- 2_pattern_printing.cpp
+++ 2_pattern_printing.cpp
@@ -62,6 +62,28 @@ int main() {
   // }
 
 
+  //Center Triangle
+  // int n=4;
+  // for(int i=0; i<n;i++)
+  // {
+  //   //space
+  //   for(int j=0; j<n-i-1; j++)
+  //   {
+  //     cout << " ";
+  //   }
+  //   //star
+  //   for(int k=0; k<2*i+1; k++)
+  //   {
+  //     cout << "*";
+  //   }
+  //   //space
+  //   for(int j=0; j<n-i-1; j++)
+  //   {
+  //     cout << " ";
+  //   }
+  //   cout << endl;
+  // }
+
   
   // End Of File
 }
