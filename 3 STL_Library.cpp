bd6c6b4b0108cdd14c0cdd89cf8e9d6c42df506c
suneetchugh-dev <valorise078gramming@gmail.com>
2026-01-29 02:54:35 +0530

    Started Off With Lecture-3 STL Library

---
+++ 3 STL_Library.cpp
@@ -0,0 +1,27 @@
+//What is STL => STL is Standard Templatee Libarary
+//STL is a collection of template classes and functions which are used for general purpose in C++ programming
+//STL is divided into 4 parts
+//1. Containers => queues, vectors, sets , map, etc
+//2. Algorithms
+//3. Iterators
+//4. Functors
+
+#include <bits/stdc++.h> //this is the library, because of this you don't have to add libraries individually
+using namespace std;
+
+// void print()
+// {
+//     cout << "Suneet Chugh";
+// }
+
+// int sum(int a, int b)
+// {
+//     cout << a+b;
+// }
+// int main()
+// {
+//     print()
+//     int s = sum(5,5);
+//     return 0;
+// }
+
