220a1250b6512e3525ada8dc280bb595de05b515
suneetchugh-dev <valorise078gramming@gmail.com>
2026-02-06 03:41:25 +0530

    Learn About Pair And Fixed File Name Error For Proper Compilation

--- 3_STL_Library.cpp
+++ 3_STL_Library.cpp
@@ -6,6 +6,8 @@
 //3. Iterators
 //4. Functors
 
+
+//=>>Let's Start
 #include <bits/stdc++.h> //this is the library, because of this you don't have to add libraries individually
 using namespace std;
 
@@ -24,4 +26,29 @@ using namespace std;
 //     int s = sum(5,5);
 //     return 0;
 // }
+
+// void explainPair() {
+
+//   //Using Pair For Storing Two Values 
+//   pair<int, int> p = {1, 3};
+//   cout << p.first << " " << p.second;
+
+//   pair<int, pair<int><int>> p ={1, {3,4}};
+//   cout << p.first <<< " " << p.second.second << " " << p.second.first
+
+//   //What If We Want To Store More Than 2 Values => then we can use nested pair
+//   pair <int, int> arr[] = { {1,2} , {2,5}, {5,1} };
+//   cout << arr[1].second;
+}
+
+
+void explainVector()
+{
+  
+}
+
+int main()
+{
+    // explainPair();
+}
 
