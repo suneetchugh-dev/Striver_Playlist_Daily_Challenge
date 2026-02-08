2b45e563ab2351c2d4ea60f97d603e4c7bac8f63
suneetchugh-dev <valorise078gramming@gmail.com>
2026-02-09 04:12:27 +0530

    Started Learning Vectors

--- 3_STL_Library.cpp
+++ 3_STL_Library.cpp
@@ -39,13 +39,34 @@ using namespace std;
 //   //What If We Want To Store More Than 2 Values => then we can use nested pair
 //   pair <int, int> arr[] = { {1,2} , {2,5}, {5,1} };
 //   cout << arr[1].second;
-}
+// }
 
 
-void explainVector()
+void explainVector() 
 {
+  // Vector are dynamic in nature, we can always increase and decrease the size
+  // of a vector, more like a container like an array but dynamic in nature
+  // vector<int> v; 
+
+  // v.push_back(1);
+  // v.emplace_back(2); //similar to push_back but faster than push_back
+
+  // vector <pair<int, int>> vec;
+
+  // v.push_back({1, 2});
+  // v.emplace_back(1, 2); //similar to push_back but faster than push_back
+
+  // vector<int> v(5,100); //vector of size 5 and all values are 100
+
+  // vector<int> v(5,20);
+
+  // cout << v[0] << " " << v[1] << " " << v[2] << " " << v[3] << " " << v[4] << endl;
+  // cout << v.size() << endl;
+
+
   
 }
+
 
 int main()
 {
