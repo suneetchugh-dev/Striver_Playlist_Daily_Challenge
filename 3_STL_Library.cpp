945d55172bf90922feb88bdf6e4d8261c5579e57
suneetchugh-dev <valorise078gramming@gmail.com>
2026-03-29 04:59:02 +0530

    Learn About List Container Type

--- 3_STL_Library.cpp
+++ 3_STL_Library.cpp
@@ -64,26 +64,84 @@
   // cout << v[0] << " " << v[1] << " " << v[2] << " " << v[3] << " " << v[4] << endl;
   // cout << v.size() << endl;
 
+ // Example: Using vector, iterators, pop_back, and erase in C++
 
-  
+//  // 1. Create a vector of integers
+//  vector<int> v;
+//  v.push_back(10); // Adds 10 to the end
+//  v.push_back(20); // Adds 20 to the end
+//  v.push_back(30); // Adds 30 to the end
+//  v.push_back(40); // Adds 40 to the end
+
+//  // 2. Print all elements using a simple for loop
+//  cout << "Vector elements using index: ";
+//  for (int i = 0; i < v.size(); i++) {
+//    cout << v[i] << " ";
+//  }
+//  cout << endl;
+
+//  // 3. Print all elements using an iterator
+//  cout << "Vector elements using iterator: ";
+//  for (vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
+//    cout << *it << " "; // *it gives the value at the iterator
+//  }
+//  cout << endl;
+
+//  // 4. Remove the last element using pop_back()
+//  v.pop_back(); // Removes 40
+//  cout << "After pop_back: ";
+//  for (int x : v)
+//    cout << x << " ";
+//  cout << endl;
+
+//  // 5. Erase the second element (index 1)
+//  // v.erase(v.begin() + 1) removes the element at index 1 (20)
+//  v.erase(v.begin() + 1);
+//  cout << "After erasing second element: ";
+//  for (int x : v)
+//    cout << x << " ";
+//  cout << endl;
+
+//  // 6. Show how to use iterator to access elements
+//  vector<int>::iterator it = v.begin(); // Points to first element
+//  cout << "First element: " << *it << endl;
+//  ++it; // Move to next element
+//  if (it != v.end())
+//    cout << "Second element: " << *it << endl;
+
+//  // 7. Safe way to move iterator forward
+//  // Only move if enough elements exist
+//  if (v.size() > 2) {
+//    it = v.begin() + 2;
+//    cout << "Third element: " << *it << endl;
+//  }
+
 // }
 
+ // Now Learning List
+ //  void explainList()
+ //  {
+    // // Remember Subscript is not allowed in List, and the benefit it provides is to
+    // // push elements in the start.
+    // list<int> ls;
+    // ls.emplace_back(4); // places at last
+    // cout << "After emplace_back(4): ";
+    // for (int x : ls)
+    //   cout << x << " ";
+    // cout << endl;
+    // ls.emplace_front(2); // places in front
+    // cout << "After emplace_front(2): ";
+    // for (int x : ls)
+    //   cout << x << " ";
+    // cout << endl;
+    // // rest all operations are same as of vector
+    // return 0;
+ // }
+
+
+
 #include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    // explainPair();
-    vector <int> v;
-    v.emplace_back(2);
-    cout << v[0] << endl;
+ using namespace std;
+ int main() {
 
-
-    vector <int>::iterator it = v.begin();
-
-    it++;
-    cout << *(it) << " ";
-    it = it + 2;
-    cout << *(it) << " ";
-    return 0;
-}
-
+ }
