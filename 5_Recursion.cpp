0ba18600e9df0210cc2bb7889c0885265b929f72
suneetchugh-dev <valorise078gramming@gmail.com>
2026-06-16 08:51:32 +0530

    learnt about recursion and base case

---
+++ 5_Recursion.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+// void f()
+// {
+//     cout << 1;
+//     f();
+// }
+// int main()
+// {
+//     f();
+// }
+
+
+// int counter=0;
+// void f()
+// {
+//     if (counter == 4)
+//     {
+//         return;
+//     }
+//     cout << counter;
+//     counter++;
+//     f();
+// }
+
+// int main()
+// {
+//     f();
+// }
