40ed7d9777082071f5dcd445535cf01f3203b7b9
suneetchugh-dev <valorise078gramming@gmail.com>
2026-06-16 08:24:54 +0530

    learnt how to get the last digit using coding

--- 4_Basic_Maths.cpp
+++ 4_Basic_Maths.cpp
@@ -1,20 +1,26 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
 
-void printLastDigit(int number) //use long long number => to print last digit of large number
-{
-    while(number>0)
-    {
-        int lastDigit = number%10;
-        number = number/10;
-        cout << "Last Digit is: " << lastDigit << endl;
-    }
-}
-int main()
-{
-    cout << "Enter your number" << endl;
-    int num; // use long long number => to print last digit of large number
-    cin >> num;
-    printLastDigit(num);
-}
+// void printLastDigit(int number) //use long long number => to print last digit of large number
+// {
+//     while(number>0)
+//     {
+//         int lastDigit = number%10;
+//         number = number/10;
+//         cout << "Last Digit is: " << lastDigit << endl;
+//     }
+// }
+// int main()
+// {
+//     cout << "Enter your number" << endl;
+//     int num; // use long long number => to print last digit of large number
+//     cin >> num;
+//     printLastDigit(num);
+// }
+
+
+
+
+
+
